394662e6e33674aa6f7bfbbc3e11b9713cec29b0
Nikol <nikolhurd@gmail.com>
2024-07-25 23:21:41 +0200

    basic arithmetic functions and operator function

---
+++ script.js
@@ -0,0 +1,16 @@
+// Basic Arithmetic Functions
+const add = (a, b) => a + b;
+
+const subtract = (a, b) => a - b;
+
+const multiply = (a, b) => a * b;
+
+const divide = (a, b) => a / b;
+
+// Operator Function
+const operate = (operator, a, b) => {
+  if (operator === "+") return add(a, b);
+  if (operator === "-") return subtract(a, b);
+  if (operator === "*") return multiply(a, b);
+  if (operator === "/") return divide(a, b);
+};
