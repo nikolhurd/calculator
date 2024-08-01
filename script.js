d35b20951273db017127428517015b9fd69152ed
Nikol <nikolhurd@gmail.com>
2024-08-01 18:00:25 +0200

    added comments and logic what happens if second num wasnt entered

--- script.js
+++ script.js
@@ -13,21 +13,22 @@ buttons.addEventListener("click", (event) => {
   let target = event.target;
 
   if (target.classList.contains("number")) {
-    currentDisplayValue += target.textContent;
-    display.textContent = currentDisplayValue;
+    // if we click on number
+    currentDisplayValue += target.textContent; // adding the numbers to the variable CDV
+    display.textContent = currentDisplayValue; // in display part of calculator we see what is inside CDV
   }
   if (target.classList.contains("operator")) {
+    // if we click on operator
     if (numValue1 !== null && currentDisplayValue !== "") {
-      numValue2 = parseFloat(currentDisplayValue);
-      console.log("num2", numValue2);
-      result = operate(operator, numValue1, numValue2);
-      numValue1 = result;
-      currentDisplayValue = "";
-      display.textContent = result.toString();
-    } else numValue1 = parseFloat(currentDisplayValue);
-    operator = target.textContent;
-    currentDisplayValue = "";
-    console.log("num1", numValue1);
+      // if we have already have first number and the user has entered a new number after the previous operation then do following
+      numValue2 = parseFloat(currentDisplayValue); // number that we have written give to the numValue2 variable
+      result = operate(operator, numValue1, numValue2); // make a calculation and give it to the result variable
+      numValue1 = result; // give a value of result to the numValue2
+      currentDisplayValue = ""; // delete what is in CDV variable
+      display.textContent = result.toString(); // make a result a string and display it in display
+    } else numValue1 = parseFloat(currentDisplayValue); // if we have not any first number, do following - number that we have written give to the numValue1 variable
+    operator = target.textContent; // remember the operator that we used
+    currentDisplayValue = ""; // delete what is in CDV variable
   }
   if (target.classList.contains("clear")) {
     currentDisplayValue = "";
@@ -37,10 +38,14 @@ buttons.addEventListener("click", (event) => {
     display.textContent = "0";
   }
   if (target.classList.contains("equals")) {
-    numValue2 = parseFloat(currentDisplayValue);
-    console.log(numValue2);
+    // if we click on equals
+    if (currentDisplayValue === "") {
+      // no new number was entered
+      numValue2 = parseFloat(numValue1); // getting the value from numValue1 to numValue2
+    } else {
+      numValue2 = parseFloat(currentDisplayValue); // else keep going with number that was entered
+    }
     result = operate(operator, numValue1, numValue2);
-    console.log(result);
     display.textContent = result.toString();
   }
 });
